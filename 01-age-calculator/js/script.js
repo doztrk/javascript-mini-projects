50fbb7b9b9b70fddd7371f128f56c38bf4987770
Jubaw <doguztrk@gmail.com>
2024-08-07 23:59:05 +0300

    day01

--- 01-age-calculator/js/script.js
+++ 01-age-calculator/js/script.js
@@ -9,6 +9,8 @@ function calculateAge() {
   const lblMonths = document.getElementById("lblMonths");
   const lblDays = document.getElementById("lblDays");
 
+  const currentDateTime = new Date();
+
   console.log(txtDay, txtMonth, txtYear);
 
   // 2 - Take the inputs and assing it to a variable
@@ -21,9 +23,51 @@ function calculateAge() {
   // falsy values: false, 0, "", null, undefined, NaN
 
   // 3 - Validate the input values
-  if (!day || !month || !year) {
-      alert("Please provide valid date");
+  if (!day || day < 1 || day > 31) {
+    alert("Invalid day");
+    return;
   }
+  if (!month || month < 1 || month > 12) {
+    alert("Invalid month");
+    return;
+  }
+  const currentYear = currentDateTime.getFullYear();
+  const currentMonth = currentDateTime.getMonth() + 1; // Zero based
+  const currentDay = currentDateTime.getDate();
+
+  if (!year || year < currentYear - 150 || year > currentYear) {
+    alert("Invalid year");
+    return;
+  }
+
   // 4 - Calculate the age
+
+  // 06-08-2024
+  // 03-06-2000
+  // ---------------
+  //  3  2   24
+  // 36-19-2023
+  // 15-10-2000
+  // ---------------
+  //  21  9   23
+
+  let ageYears = currentYear - year;
+  let ageMonths = currentMonth - month;
+  let ageDays = currentDay - day;
+
+  if (ageDays < 0) {
+    ageDays += 30;
+    ageMonths--;
+  }
+  if (ageMonths < 0) {
+    ageMonths += 12;
+    ageYears--;
+  }
+  console.log(ageYears, ageMonths, ageDays);
+
   // 5 - Display the result in the HTML
+
+  lblYears.innerHTML = ageYears;
+  lblMonths.innerHTML = ageMonths;
+  lblDays.innerHTML = ageDays;
 }
